a669b24c90e07ec6b169950aaa41cbffd3af67b1
WilliamCruuz <cruzwilliam14@gmail.com>
2024-04-14 21:09:04 -0300

    Conluindo atividade 9

--- Atv8/Q1/script.js
+++ Atv8/Q1/script.js
@@ -1,11 +1,10 @@
 var readline = require('readline-sync');
 
 function somatorio(numero) {
-
-    return numero === 1 ? 1 : numero + somatorio(numero - 1)
+    numero == 1 ? numero + 1 : somatorio(numero - 1);
+    
     
 }
 
-let numero = Number(readline.question('Digite um numero inteiro qualquer: ')); 
+let numero = readline.question('Digite um numero inteiro qualquer: ');
 console.log(somatorio(numero))
-
